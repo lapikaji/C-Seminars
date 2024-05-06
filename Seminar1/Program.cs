c7d0df0685350da553e1361efdfd3974c6449046
lapikag <lapikag@gmail.com>
2024-05-06 17:11:10 +0200

    add Homework folder

---
+++ Seminar1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 7 == 0 && a % 23 == 0)
+        {
+            Console.WriteLine("Кратно");
+        }
+        else
+        {
+            Console.WriteLine("Не кратно");
+        }
+    }
+}
