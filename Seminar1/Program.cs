17482310e7c01d21576c3c67023c3b2cab4ac378
lapikag <lapikag@gmail.com>
2024-05-06 17:36:41 +0200

    add Seminar1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿// Console.WriteLine("Input number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Quad of {a} -> {a * a}");
+
+// Вычислить, является ли второе число квадратом от первого:
+
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 / num2 == num2)
+// {
+//     Console.WriteLine($"{num1} IsSquareOf {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($"{num1} IsNotSquareOf {num2}");
+// }
+
+// Вывести все натуральные числа от минус N до плюс N
+
+// Console.Write("Input N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int start = -n;
+
+// while (start <= n)
+// {
+//     Console.Write($"{start} ");
+//     start++; //start = start + 1; 
+// }
+
+// Написать программу, которая принимает на вход целое трехзначное число и на выходе покахывает сумму первой и третьей цифры
+
+// Console.Write("Input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n >= 100 && n <= 999)
+// {
+//     int firstDigit = n / 100;
+//     int secondDigit = (n / 10) % 10;
+//     int thirdDigit = n % 10;
+//     int sum = firstDigit + thirdDigit;
+//     Console.WriteLine(sum);
+// }
+// else
+// {
+//     Console.WriteLine("Wrong input");
+// }
+
